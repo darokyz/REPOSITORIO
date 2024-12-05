e3a90c9c3404602ffa9b9cab0851401d438b0f45
darokyz <otaviocarlos299@hotmail.com>
2024-12-05 10:59:51 -0300

    correçao do desafio
    correçao do desafio particionamento pro data json, eu estava particionando pelas datas contidas dentro do arquivo, mas corrigi agora para particionar com base na data de execuçao do arquivo

--- SPRINT8/desafio/glue_json_to_parquet.py
+++ SPRINT8/desafio/glue_json_to_parquet.py
@@ -1,4 +1,5 @@
 import sys
+from datetime import datetime
 from awsglue.transforms import *
 from awsglue.utils import getResolvedOptions
 from pyspark.context import SparkContext
@@ -12,7 +13,12 @@ glueContext = GlueContext(sc)
 spark = glueContext.spark_session
 
 RAW_JSON_PATH = "s3://otaviodesafio/Raw/TMDB/JSON/action_movies_with_runtime.json"
-TRUSTED_PARQUET_PATH = "s3://otaviodesafio/Trusted/TMDB/API/"
+
+execution_date = datetime.now()
+execution_date_str = execution_date.strftime("%Y-%m-%d")
+execution_path = execution_date.strftime("%Y/%m/%d")
+TRUSTED_PARQUET_PATH = f"s3://otaviodesafio/Trusted/TMDB/API/{execution_path}/"
+output_file_name = f"action_movies_{execution_date_str}.parquet"
 
 datasource = glueContext.create_dynamic_frame.from_options(
     format="json",
@@ -22,7 +28,12 @@ datasource = glueContext.create_dynamic_frame.from_options(
 
 df = datasource.toDF()
 
-df = df.withColumn("release_date", to_date(col("release_date"), "yyyy-MM-dd"))
+df = df.withColumn(
+    "release_date",
+    when(to_date(col("release_date"), "yyyy-MM-dd").isNotNull(), to_date(col("release_date"), "yyyy-MM-dd"))
+    .otherwise(lit(None))
+)
+
 df = df.withColumn("title", when(col("title").isNotNull(), col("title")).otherwise("Unknown"))
 df = df.withColumn("runtime", when(col("runtime").isNotNull(), col("runtime")).otherwise(0))
 df = df.withColumn("vote_count", when(col("vote_count").isNotNull(), col("vote_count")).otherwise(0))
@@ -32,8 +43,8 @@ df = df.withColumn("overview", when(col("overview").isNotNull(), col("overview")
 df = df.withColumn("original_language", when(col("original_language").isNotNull(), col("original_language")).otherwise("Unknown"))
 df = df.withColumn("production_countries", when(col("production_countries").isNotNull() & (col("production_countries") != array()), col("production_countries")).otherwise(array(lit("Unknown"))))
 df = df.withColumn("director", when(col("director").isNotNull(), col("director")).otherwise("Unknown"))
-
 df = df.filter(col("release_date").isNotNull())
+df = df.withColumn("execution_date", lit(execution_date_str))
 
 transformed_dynamic_frame = DynamicFrame.fromDF(df, glueContext, "transformed_dynamic_frame")
 
@@ -42,7 +53,12 @@ glueContext.write_dynamic_frame.from_options(
     connection_type="s3",
     connection_options={
         "path": TRUSTED_PARQUET_PATH,
-        "partitionKeys": ["release_date"],
+        "partitionKeys": ["execution_date"],
     },
     format="parquet"
 )
+
+output_file_path = f"{TRUSTED_PARQUET_PATH}{output_file_name}"
+df.write.mode("overwrite").parquet(output_file_path)
+
+ # foi acrescentada algumas mudanças nesse codigo pois a execuçao do codigo antigo estava particionando o arquivo pelas datas contidas no conteudo dele "realease_date" mas esse ja esta atualizado para data de execuçao
